dd23ed9670fd68e337427b9e8aebd23360948175
Karol Chmiel <karolch94@gmail.com>
2022-04-12 14:57:28 +0200

    feat(simverse): add user stats

--- contracts/simverse.sol/SimFactory.sol
+++ contracts/simverse.sol/SimFactory.sol
@@ -11,10 +11,24 @@ contract SimFactory is Ownable {
 
     struct Sim {
         string name;
-        uint256 dna;
+        uint128 dna;
+        uint16 strength;
+        uint16 perception;
+        uint16 endurance;
+        uint16 charisma;
+        uint16 intelligence;
+        uint16 agility;
+        uint16 luck;
+        uint16 level;
+        uint256 readyTime;
     }
 
     Sim[] public sims;
+
+    function createRandomSim(string _name) public {
+        uint256 randDna = _generateRandomDna(_name);
+        _createSim(_name, randDna);
+    }
 
     function _createSim(string _name, uint256 _dna) private {
         uint256 id = sims.push(Sim(_name, _dna)) - 1;
@@ -24,10 +38,5 @@ contract SimFactory is Ownable {
     function _generateRandomDna(string _str) private view returns (uint256) {
         uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
-    }
-
-    function createRandomSim(string _name) public {
-        uint256 randDna = _generateRandomDna(_name);
-        _createSim(_name, randDna);
     }
 }
